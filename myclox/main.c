0272a5879111a1b1077aae81990ee684719674fd
babaiserror <kprojab@gmail.com>
2022-12-29 22:59:50 -0800

    Finished chapter 15.

--- myclox/main.c
+++ myclox/main.c
@@ -2,18 +2,32 @@
 #include "chunk.h"
 #include "debug.h"
 #include "value.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
+    
     Chunk chunk;
     initChunk(&chunk);
 
+    /*
     for (int i = 0; i < 300; i++) {
 	writeConstant(&chunk, 1.2, i/2);
-    }
+    }*/
+    
+    writeConstant(&chunk, 1.2, 123);
 
+    writeConstant(&chunk, 3.4, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+    writeConstant(&chunk, 5.6, 123);
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+    
     writeChunk(&chunk, OP_RETURN, 500);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
---
+++ myclox/vm.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "common.h"
+#include "vm.h"
+#include "chunk.h"
+#include "debug.h"
+#include "value.h"
+
+
+VM vm;
+
+static void resetStack() { vm.stackTop = vm.stack; }
+
+void initVM() {
+    resetStack();  
+}
+
+void freeVM() {}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+Value peek() { return *(vm.stackTop - 1); }
+
+void replaceTop(Value value) {
+    *(vm.stackTop - 1) = value;
+}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_LONG_CONSTANT() (vm.chunk->constants.values[READ_BYTE() | \
+			      (READ_BYTE() << 8) | (READ_BYTE() << 16)])
+#define BINARY_OP(op) \
+    do { \
+	double b = pop(); \
+        replaceTop(peek() op b); \
+    } while (false)
+    
+    for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+	printf("          ");
+	for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+	    printf("[ ");
+	    printValue(*slot);
+	    printf(" ]");
+	}
+	printf("\n");
+	disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+	uint8_t instruction;
+	switch (instruction = READ_BYTE()) {
+	case OP_CONSTANT: {
+	    Value constant = READ_CONSTANT();
+	    push(constant);
+	    break;
+	}
+	case OP_CONSTANT_LONG: {
+	    Value constant = READ_LONG_CONSTANT();
+	    push(constant);
+	    break;
+	}
+	case OP_ADD:      BINARY_OP(+); break;
+	case OP_SUBTRACT: BINARY_OP(-); break;
+	case OP_MULTIPLY: BINARY_OP(*); break;
+	case OP_DIVIDE:   BINARY_OP(/); break;
+	case OP_NEGATE:   replaceTop(-peek()); break;
+	case OP_RETURN: {
+	    printValue(pop());
+	    printf("\n");
+	    return INTERPRET_OK;
+	}
+	}
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+#undef READ_LONG_CONSTANT
+#undef BINARY_OP
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
