09e5727cd3c93ff44026d25d2924daa8d95961ad
babaiserror <kprojab@gmail.com>
2023-01-05 00:52:06 -0800

    Working on chapter 26.

--- myclox/memory.c
+++ myclox/memory.c
@@ -6,7 +6,18 @@
 #include "value.h"
 #include "vm.h"
 
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize > oldSize) {
+#ifdef DEBUG_STRESS_GC
+	collectGarbage();
+#endif
+    }
+    
     if (newSize == 0) {
         free(pointer);
         return NULL;
@@ -17,8 +28,33 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     return result;
 }
 
+void markObject(Obj* object) {
+    if (object == NULL) return;
+#ifdef DEBUG_LOG_GC
+    printf("%p mark ", (void*)object);
+    printValue(OBJ_VAL(object));
+    printf("\n");
+#endif
+    
+    object->isMarked = true;
+}
+
+void markValue(Value value) {
+    if (IS_OBJ(value)) markObject(AS_OBJ(value));
+}
+
 static void freeObject(Obj* object) {
+#ifdef DEBUG_LOG_GC
+    printf("%p free type %d\n", (void*)object, object->type);
+#endif
+    
     switch (object->type) {
+    case OBJ_CLOSURE: {
+	ObjClosure* closure = (ObjClosure*)object;
+	FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
+	FREE(ObjClosure, object);
+	break;
+    }
     case OBJ_FUNCTION: {
 	ObjFunction* function = (ObjFunction*)object;
 	freeChunk(&function->chunk);
@@ -34,7 +70,28 @@ static void freeObject(Obj* object) {
 	FREE(ObjString, object);
         break;
     }
+    case OBJ_UPVALUE:
+	FREE(ObjUpvalue, object);
+	break;
     }
+}
+
+static void markRoots() {
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+	markValue(*slot);
+    }
+}
+
+void collectGarbage() {
+#ifdef DEBUG_LOG_GC
+    printf("-- gc begin\n");
+#endif
+
+    markRoots();
+
+#ifdef DEBUG_LOG_GC
+    printf("-- gc end\n");
+#endif
 }
 
 void freeObjects() {
